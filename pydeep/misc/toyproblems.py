d07f34fff52fede4b4e91677fb4d4ce7069ca3d5
Jose Pinilla <jpinilla@ece.ubc.ca>
2019-12-06 17:38:05 -0800

    Tests pass in 2.7. New wait for Travis.

--- pydeep/misc/toyproblems.py
+++ pydeep/misc/toyproblems.py
@@ -107,7 +107,8 @@ def generate_bars_and_stripes_complete(length):
     bars = bars.reshape(2 ** length * length, 1)
     bars = numx.repeat(bars, length, 1)
     bars = bars.reshape(2 ** length, length * length)
-    return numx.vstack((stripes, bars))
+    return numx.vstack((stripes[0:stripes.shape[0]-1],bars[1:bars.shape[0]]))
+    # return numx.vstack((stripes, bars))
 
 
 def generate_shifting_bars(length,
